a2f8d05fb7ea5d525b67a22c53aa6f6c415828c0
benjameep <14208829+benjameep@users.noreply.github.com>
2024-08-27 11:34:56 -0600

    add code mirror back in

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 import './style.css'
 import { EditorView, basicSetup } from 'codemirror'
 
-// import content from 'content'
-
 // Load Script into editor
+const $editor = document.getElementById('editor')
+const code = $editor.innerText
+$editor.innerHTML = ''
 window.editor = new EditorView({
-  doc: content[0].models[0].code,
+  doc: code,
   extensions: basicSetup,
-  parent: document.getElementById('editor'),
+  parent: $editor,
 })
-
-// Load Lesson Content
-document.querySelector('#lesson .content').innerHTML = content[0].html
