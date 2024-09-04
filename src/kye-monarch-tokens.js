8d2340873ef94f8b0fc1ca7fcce64effdb88d36b
benjameep <14208829+benjameep@users.noreply.github.com>
2024-09-04 15:30:25 -0600

    use monaco instead of codemirror

---
+++ src/kye-monarch-tokens.js
@@ -0,0 +1,64 @@
+// https://microsoft.github.io/monaco-editor/monarch.html
+
+export default {
+  defaultToken: 'invalid',
+  keywords: [
+    'if','else','assert','return','operation','to','from','null',
+    'in','is','and','or','not',
+    'TRUE','FALSE','NULL',
+    'this'
+  ],
+  
+  operators: [
+    '+', '-', '*', '/', '%', '==', '!=', '>', '<', '>=', '<=', '&', '|', '!', '=',
+  ],
+  
+  // we include these common regular expressions
+  symbols:  /[=><!~?:&|+\-*\/\^%]+/,
+  
+  // C# style strings
+  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+  tokenizer: {
+    root: [
+      // identifiers and keywords
+      [/[a-z_$][\w$]*/, { cases: { '@keywords': 'keyword',
+                                   '@default': 'variable' } }],
+      [/[A-Z_$][\w$]*/, { token: 'type.identifier' } ],
+
+      // whitespace
+      { include: '@whitespace' },
+
+      // delimiters and operators
+      [/[{}()\[\]]/, '@brackets'],
+      [/@symbols/, { cases: { '@operators': 'operator',
+                              '@default'  : '' } } ],
+                              
+      // numbers
+      [/\d*\.\d+([eE][\-+]?\d+)?/, 'number.float'],
+      [/\d+/, 'number'],
+
+      // strings
+      [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
+      [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
+    ],
+
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/#.*$/,    'comment'],
+    ],
+
+    format: [
+      [/>/, { token: 'format', bracket: '@close', next: '@pop' }],
+      [/[^<>]+/, 'format'],
+    ],
+
+    string: [
+      [/[^\\"]+/,  'string'],
+      [/@escapes/, 'string.escape'],
+      [/\\./,      'string.escape.invalid'],
+      [/"/,        { token: 'string.quote', bracket: '@close', next: '@pop' } ]
+    ],
+
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,15 +1,9 @@
 import './style.css'
 import './vscode-dark.css'
 import 'codemirror/lib/codemirror.css'
-import { EditorView, basicSetup } from 'codemirror'
-// import { oneDark } from "@codemirror/theme-one-dark";
-// import { kye } from './long-gone'
+import loader from '@monaco-editor/loader'
 import Prism from 'prismjs'
-// import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
-// import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
-import CodeMirror from 'codemirror/lib/codemirror'
-import 'codemirror/addon/mode/simple'
-import 'codemirror/addon/edit/matchbrackets'
+import kyeMonarchTokens from './kye-monarch-tokens'
 
 Prism.languages.kye = {
   comment: {
@@ -27,32 +21,18 @@ Prism.languages.kye = {
   variable: /\b[a-z_]+[a-z_A-Z0-9]*\b/,
 }
 
-CodeMirror.defineSimpleMode('kye', {
-  start: [
-    { regex: /#.*/, token: 'comment' },
-    { regex: /"(?:[^\\]|\\.)*?"/, token: 'string'},
-    { regex: /(?:\d*\.\d+|\d+\.\d*|\d+)/, token: 'number' },
-    { regex: /(?:if|else|assert|operation|to|from|null)\b/, token: 'keyword' },
-    { regex: /\b(this|super)\b/, token: 'atom' },
-    // { regex: /@([a-z]\w*\.)*[a-z]\w*/, token: 'symbol' },
-    // { regex: /[A-Z_]+[A-Z0-9_]*/, token: 'variable-3' },
-    { regex: /\b[A-Z]+[a-z][a-zA-Z0-9]*\b/, token: 'type' },
-    { regex: /\b[a-z_]+[a-z_A-Z0-9]*\b/, token: 'property' },
-  ],
-})
-
 window.addEventListener('DOMContentLoaded', () => {
   const $editor = document.getElementById('editor')
   const code = $editor.querySelector('pre').innerText
   $editor.removeChild($editor.querySelector('pre'))
-  window.cm = CodeMirror($editor, {
-    value: code,
-    theme: 'vscode-dark',
-    mode: 'kye',
-    lineNumbers: true,
-    lineWrapping: true,
-    tabSize: 2,
-    matchBrackets: true,
+  loader.init().then(monaco => {
+    monaco.languages.register({ id: 'kye' });
+    monaco.languages.setMonarchTokensProvider('kye', kyeMonarchTokens)
+    monaco.editor.create($editor, {
+      value: code,
+      language: 'kye',
+      theme: 'vs-dark',
+    })
   })
 })
 
