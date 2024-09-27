cd6aef9529b3aa6fa0ce85c86b472868188930ad
benjameep <14208829+benjameep@users.noreply.github.com>
2024-09-27 12:45:18 -0600

    catch errors thrown by Kye

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from kye.kye import Kye
 from kye.compiled import Compiled
-import json
 import dataclasses
 
 print('ready')
@@ -9,26 +8,34 @@ print('ready')
 __all__ = ['compile', 'validate']
 
 def compile(code):
-    kye = Kye()
-    if kye.compile(code):
-      return {
-        'compiled': kye.compiled.to_dict(),
-      }
-    else:
+    try:
+      kye = Kye()
+      if kye.compile(code):
+        return {
+          'compiled': kye.compiled.to_dict(),
+        }
+      else:
+        return {
+          'errors': [
+            dataclasses.asdict(err)
+            for err in kye.reporter.errors
+          ]
+        }
+    except Exception as e:
+      print(e)
+      return { 'errors': [] }
+
+def validate(compiled, data, model_name):
+    try:
+      kye = Kye()
+      kye.load_compiled(Compiled.from_dict(compiled))
+      kye.load_df(model_name, pd.DataFrame(data))
       return {
         'errors': [
           dataclasses.asdict(err)
           for err in kye.reporter.errors
         ]
       }
-
-def validate(compiled, data, model_name):
-    kye = Kye()
-    kye.load_compiled(Compiled.from_dict(compiled))
-    kye.load_df(model_name, pd.DataFrame(data))
-    return {
-      'errors': [
-        dataclasses.asdict(err)
-        for err in kye.reporter.errors
-      ]
-    }
+    except Exception as e:
+      print(e)
+      return { 'errors': [] }
